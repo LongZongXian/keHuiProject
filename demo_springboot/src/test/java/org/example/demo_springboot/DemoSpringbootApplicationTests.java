dc928fa5ebab544183e9ff401aed87cd7ba7a61c
LongZongXian <18885090900@163.com>
2024-10-18 13:29:28 +0800

    初始化_springboot

--- demo_springboot/src/test/java/org/example/demo_springboot/DemoSpringbootApplicationTests.java
+++ demo_springboot/src/test/java/org/example/demo_springboot/DemoSpringbootApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DemoSpringbootApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
